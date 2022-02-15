3a670b3f6c436b78afa38a3f4b1008f6994454a0
felipeolvr <luizfelipe.lins@gmail.com>
2022-02-15 14:00:15 -0400

    add(pages): exibirCharacter
    *criado o componente para exibir os dados do personagem

---
+++ src/pages/exibirCharacter.vue
@@ -0,0 +1,17 @@
+<template>
+  <h2>chegou aqui</h2>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "exibirCharacter",
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style>
+</style>
