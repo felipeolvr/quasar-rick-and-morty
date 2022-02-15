1c09ecd822ea71354427e03690b8293096d0d086
felipeolvr <luizfelipe.lins@gmail.com>
2022-02-15 14:43:07 -0400

    refactor(pages): exibirCharacter
    *feito a requisicao para trazer os dados do personagem

--- src/pages/exibirCharacter.vue
+++ src/pages/exibirCharacter.vue
@@ -1,14 +1,48 @@
 <template>
   <h2>chegou aqui</h2>
+  {{ data }}
 </template>
 
 <script>
 import axios from "axios";
+import { ref, onMounted } from "vue";
+import { useRoute } from "vue-router";
 
 export default {
   name: "exibirCharacter",
   setup() {
-    return {};
+    const route = useRoute();
+
+    const data = ref(null);
+
+    function getCharacter() {
+      const endpoint = "https://rickandmortyapi.com/graphql";
+
+      const graphqlQuery = {
+        query: `query {
+  character(id: "${route.params.id}") {
+		id, name, image, location{
+			name
+		},
+		episode {
+			id, name
+		}
+	}
+}`,
+      };
+
+      axios.post(endpoint, graphqlQuery).then((response) => {
+        data.value = response.data.data.character;
+        console.log(response.data.data.character);
+      });
+    }
+    onMounted(async () => {
+      getCharacter();
+    });
+
+    return {
+      data,
+    };
   },
 };
 </script>
