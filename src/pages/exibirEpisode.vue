f05fcbf5ad56c6f0a2201643bfac9e43b60638e6
felipeolvr <luizfelipe.lins@gmail.com>
2022-02-23 16:06:06 -0400

    add(page): exibir Episodio

---
+++ src/pages/exibirEpisode.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="q-pa-md">
+    <q-card class="my-card"> chegou aqui </q-card>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import { ref, onMounted } from "vue";
+import { useRoute } from "vue-router";
+
+export default {
+  name: "exibirEpisode",
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style>
+</style>
